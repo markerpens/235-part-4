aad9b8cacc0f058f7e826cd96887722a6621208a
markerpens <48663474+markerpens@users.noreply.github.com>
2019-03-28 22:28:01 -0700

    Add files via upload

---
+++ Your Beef/js/main.js
@@ -0,0 +1,19 @@
+//For laptops, allows swiping up to scroll horizontally. Mousewheel also scrolls.
+
+jQuery(function ($) {
+   $.fn.hScroll = function (amount) {
+       amount = amount || 120;
+       $(this).bind("DOMMouseScroll mousewheel", function (event) {
+           var oEvent = event.originalEvent,
+               direction = oEvent.detail ? oEvent.detail * -amount : oEvent.wheelDelta,
+               position = $(this).scrollLeft();
+           position += direction > 0 ? -amount : amount;
+           $(this).scrollLeft(position);
+           event.preventDefault();
+       })
+   };
+});
+
+$(document).ready(function() {
+   $('#box').hScroll(60); // You can pass (optionally) scrolling amount
+});
